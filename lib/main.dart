a10be969a95e95ed1358909c93cec9dc7b4b8763
Subrath <subrathitachi6@gmail.com>
2024-10-04 19:20:11 +0530

    getting started app

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  var another = "Hello world";
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+        body: Column(children: [
+      Text(appState.another),
+      Text('A random text:'),
+      Text(appState.current.asLowerCase),
+      ElevatedButton(
+          onPressed: () {
+            appState.getNext();
+          },
+          child: Text('Next please'))
+    ]));
+  }
+}
